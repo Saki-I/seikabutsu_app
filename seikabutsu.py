7291e788c8058a4be35b2631e4c52d9b1237099b
Your Name <you@example.com>
2023-04-17 21:44:50 +0900

    initial commit

---
+++ seikabutsu.py
@@ -0,0 +1,58 @@
+
+
+from PIL import Image
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+import pickle
+import numpy as np
+classes = ["0","1","2","3","4","5","6","7","8","9"]
+image_size = 8
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            image = Image.open(filepath).convert("L")
+            image = image.resize((8,8), Image.ANTIALIAS)
+            img = np.asarray(image, dtype=float)
+            img = 16 - np.floor(17*img/256)
+            img = img.flatten()
+            predicted = model.predict([img])[0]
+            print(model.predict([img]))
+            pred_answer = "これは " + classes[predicted] + " です"
+
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    app.run()
